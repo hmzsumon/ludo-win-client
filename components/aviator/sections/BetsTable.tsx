e90a39b0edc2f568716c49de36e5cf10862acb77
Zakaria Sumon <zsumonn@gmail.com>
2026-09-20 03:09:28 +0600

    update

--- components/aviator/sections/BetsTable.tsx
+++ components/aviator/sections/BetsTable.tsx
@@ -65,7 +65,7 @@ export default function BetsTable({
                 }}
               />
               <span className="truncate">
-                {bet.player.replace(/\s·\sBOT$/, "")}
+                {bet.player.replace(/\s·\s$/, "")}
               </span>
             </span>
             <span className="text-right">{bet.amount.toFixed(2)}</span>
